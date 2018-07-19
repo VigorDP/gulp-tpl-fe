c98f8942cd61baac5f3c1e52736cfc85eae64edf
fengyinchao <1542963276@qq.com>
2018-07-19 23:37:29 +0800

    modify imgMin.js

--- gulp/tasks/common/imgMin.js
+++ gulp/tasks/common/imgMin.js
@@ -3,21 +3,31 @@
  */
 
 const gulp = require('gulp'),
-	config = require('../../../config.js'),
-	gulpIf = require('gulp-if'),
-	gulpSmushit = require('gulp-smushit'),
-	gulpNewer = require('gulp-newer'), //增量更新
-	gulpLogger = require('gulp-logger'); //打印增量内容
+  config = require('../../../config.js'),
+  gulpIf = require('gulp-if'),
+  del = require('del'),
+  gulpSmushit = require('gulp-smushit'),
+  gulpNewer = require('gulp-newer'), //增量更新
+  gulpLogger = require('gulp-logger'); //打印增量内容
 
 gulp.task('imgMin', function() {
-	return gulp.src(config.src + 'static/img/**')
-		.pipe(gulpNewer(config.temp + "static/img"))
-		.pipe(gulpLogger({
-			showChange: true
-		}))
-		.pipe(gulpIf(config.isImgOptmize, gulpSmushit({
-			verbose: true
-		})))
-		.pipe(gulp.dest(config.temp + 'static/img'))
-		// .pipe(gulp.dest(config.dist + 'sce/app/static/img'));
+  del.sync(config.temp + 'static/img/**');
+  return gulp
+    .src(config.src + 'static/img/**')
+    .pipe(gulpNewer(config.temp + 'static/img'))
+    .pipe(
+      gulpLogger({
+        showChange: true
+      })
+    )
+    .pipe(
+      gulpIf(
+        config.isImgOptmize,
+        gulpSmushit({
+          verbose: true
+        })
+      )
+    )
+    .pipe(gulp.dest(config.temp + 'static/img'));
+  // .pipe(gulp.dest(config.dist + 'sce/app/static/img'));
 });
