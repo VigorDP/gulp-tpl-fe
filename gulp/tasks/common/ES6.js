6596c58746b563f030e05572f7bfeb8a538fa9b3
fengyinchao <1542963276@qq.com>
2018-07-21 13:27:00 +0800

    es6.js update

--- gulp/tasks/common/ES6.js
+++ gulp/tasks/common/ES6.js
@@ -3,34 +3,40 @@
  */
 
 let gulp = require('gulp'),
-	config = require('../../../config.js'),
-	gulpBabel = require('gulp-babel'),
-	gulpNewer = require('gulp-newer'), //增量更新
-	gulpLogger = require('gulp-logger'); //打印增量内容
+  config = require('../../../config.js'),
+  gulpBabel = require('gulp-babel'),
+  gulpNewer = require('gulp-newer'), //增量更新
+  gulpLogger = require('gulp-logger'); //打印增量内容
 
-let {
-	NODE_ENV
-} = process.env;
+let { NODE_ENV } = process.env;
 
 gulp.task('ES6', function() {
-	return gulp.src([config.src + 'static/es6/**'])
-		.pipe(gulpBabel({
-			presets: [
-				['env', {
-					"targets": {
-						browsers: ["safari>=9", "android>=5", "ios>=9"] //可取值：chrome, opera, edge, firefox, safari, ie, ios, android, node, electron.
-					},
-					"modules": 'umd', //可取值"amd" | "umd" | "systemjs" | "commonjs" | false, defaults to "commonjs".
-					"useBuiltIns": true, //使用'babel-polyfill'
-					"debug": false //这里按server环境来区分是否debug有些欠妥 以后遇到问题再改 @liuxuefeng 20180306
-				}]
-			],
-			ignore: [config.src + 'static/es6/lib/**']
-			// plugins: ['transform-runtime']
-		}))
-		.pipe(gulpNewer(config.temp + "static/js"))
-		.pipe(gulpLogger({
-			showChange: true
-		}))
-		.pipe(gulp.dest(config.temp + 'static/js'))
+  return gulp
+    .src([config.src + 'static/es6/**'])
+    .pipe(
+      gulpBabel({
+        presets: [
+          [
+            'env',
+            {
+              targets: {
+                browsers: ['safari>=9', 'android>=5', 'ios>=9'] //可取值：chrome, opera, edge, firefox, safari, ie, ios, android, node, electron.
+              },
+              modules: 'umd', //可取值"amd" | "umd" | "systemjs" | "commonjs" | false, defaults to "commonjs".
+              useBuiltIns: false, //使用'babel-polyfill'
+              debug: false //这里按server环境来区分是否debug有些欠妥 以后遇到问题再改 @liuxuefeng 20180306
+            }
+          ]
+        ],
+        ignore: [config.src + 'static/es6/lib/**'],
+        plugins: ['transform-runtime']
+      })
+    )
+    .pipe(gulpNewer(config.temp + 'static/js'))
+    .pipe(
+      gulpLogger({
+        showChange: true
+      })
+    )
+    .pipe(gulp.dest(config.temp + 'static/js'));
 });
