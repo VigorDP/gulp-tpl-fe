fcf4adfdc676d1e6d4a40e9f85f9ebd702ec392a
fengyinchao <fengyinchao@shimo.im>
2018-07-22 13:54:16 +0800

    format

--- src/static/es6/index.js
+++ src/static/es6/index.js
@@ -1,4 +1,11 @@
 /* eslint-disable */
-const a = { name: 123, age: 345, name1: 123, age1: 345, na2me: 123, ag2e: 345 };
+const a = {
+  name: 123,
+  age: 345,
+  name1: 123,
+  age1: 345,
+  na2me: 123,
+  ag2e: 345
+};
 
 var fn = p => alert(3);
